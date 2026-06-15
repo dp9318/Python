894d01490f03fa9156f81d131c1e5ed021c3b261
Dipankar Kumar <dipankarkumar626@gmail.com>
2026-06-16 01:03:55 +0530

    data types and type casting in py

---
+++ Basics/setting_specific_datatype.py
@@ -0,0 +1,19 @@
+# setting specific data type in a variable
+# variable  dataType            
+x =         str("Hello World")	
+x =         int(20)	
+x =         float(20.5)		
+x =         complex(1j)		
+x =         (("apple", "banana", "cherry"))		
+x =         tuple(("apple", "banana", "cherry"))		
+x =         range(6)		
+x =         dict(name="John", age=36)		
+x =         set(("apple", "banana", "cherry"))		
+x =         frozenset(("apple", "banana", "cherry"))		
+x =         bool(5)		
+x =         bytes(5)		
+x =         bytearray(5)		
+x =         memoryview(bytes(5))	
+
+
+# above assignment of variableand data is also known as typecasting
